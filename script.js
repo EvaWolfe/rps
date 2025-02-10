357fda816a5ff685be16c38012b661ee1248fb6b
EvaWolfe <eywolfe@gmail.com>
2025-02-10 17:28:40 -0500

    updates js

--- script.js
+++ script.js
@@ -7,49 +7,77 @@ let playerChoice;
 let computerChoice;
 let playerScore = 0;
 let computerScore = 0;
-
-//get player's choice
-function getPlayerChoice() {
-  //alert so player can input their choice
-  playerChoice = window.prompt('Choose your weapon: R, P, or S!');
-  //failsafe for invalid choices 
-  if (!choices.includes(playerChoice)) {
-    window.prompt('You can only choose from R, P, or S...try again...');
-  }
+const tracking = {
+  wins: 0,
+  losses: 0,
+  ties: 0,
+  rock: 0,
+  paper: 0,
+  scissors: 0
 };
-getPlayerChoice();
 
-//computer choice - random
-function randomComputerChoice() {
-  const randomChoice = Math.floor(Math.random() * choices.length);
-  return choices[randomChoice];
-};
-randomComputerChoice();
 
-//figure out the winner
-function determineWinner() {
-  //tie
-  if (playerChoice === computerChoice) {
-    window.alert(`It's a tie!`);
-    //player wins
-  } else if (
-    (playerChoice === 'r' && computerChoice === 's') ||
-    (playerChoice === 'p' && computerChoice === 'r') ||
-    (playerChoice === 's' && computerChoice === 'p')
-  ) {
-    window.alert('You win!');
-    //computer wins
-  } else {
-    window.alert(`Computer Wins!`)
-  }
-};
-determineWinner();
+function game() {
+  //get player's choice
+  function getPlayerChoice() {
+    //alert so player can input their choice
+    playerChoice = window.prompt('Choose your weapon: R, P, or S!');
+    //failsafe for invalid choices 
+    if (!choices.includes(playerChoice)) {
+      window.prompt('You can only choose from R, P, or S...try again...');
+    };
 
-//play again?
-function playagain() {
-  window.prompt(`Would you like to play again?` === `y`){
-    reload();
-  }else {
-    window.alert('Thanks for playing!');
-  }
+    //tracks choices made by player
+    function tracking() {
+      if (playerChoice === 'r') {
+        tracking.rock++;
+      } else if (playerChoice === 'p') {
+        tracking.paper++;
+      } else {
+        tracking.scissors++;
+      }
+      return;
+    };
+    tracking();
+  };
+  getPlayerChoice();
+
+  //computer choice - random
+  const compRandom = Math.floor(Math.random() * choices.length);
+  computerChoice = choices[compRandom];
+  window.alert(`Computer chose: ${computerChoice}!`);
+
+
+  //figure out the winner
+  function determineWinner() {
+    //tie
+    if (playerChoice === computerChoice) {
+      window.alert(`It's a tie!`);
+      tracking.ties++;
+      //player wins
+    } else if (
+      (playerChoice === 'r' && computerChoice === 's') ||
+      (playerChoice === 'p' && computerChoice === 'r') ||
+      (playerChoice === 's' && computerChoice === 'p')
+    ) {
+      window.alert('You win!');
+      tracking.wins++;
+      //computer wins
+    } else {
+      window.alert(`Computer Wins!`)
+      tracking.losses++;
+    }
+  };
+  determineWinner();
+
+  //play again?
+  function playagain() {
+    if (window.confirm(`Would you like to play again?`)) {
+      game();
+    } else {
+      window.alert('Thanks for playing!');
+    }
+  };
+  playagain();
 };
+game();
